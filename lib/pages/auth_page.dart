0ef0d32b6aa87dbb323c2ee9d395cdff83fe61e7
Jhoan Mauricio Vivas Rubiano <raiman114@gmail.com>
2022-05-10 15:59:42 -0500

    Adding build in animation widgets.

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -106,7 +106,8 @@ class _AuthCardState extends State<AuthCard>
   final _passwordController = TextEditingController();
 
   AnimationController _controllerAnimation;
-  Animation<Size> _heightAnimantion;
+  Animation<Offset> _slideAnimantion;
+  Animation<double> _opacityAnimation;
 
   @override
   void initState() {
@@ -116,11 +117,15 @@ class _AuthCardState extends State<AuthCard>
       duration: Duration(milliseconds: 300),
     );
 
-    _heightAnimantion = Tween<Size>(
-            begin: Size(double.infinity, 260), end: Size(double.infinity, 320))
+    _slideAnimantion = Tween<Offset>(begin: Offset(0, -1.5), end: Offset(0, 0))
         .animate(CurvedAnimation(
       parent: _controllerAnimation,
       curve: Curves.fastOutSlowIn,
+    ));
+    _opacityAnimation =
+        Tween<double>(begin: 0.0, end: 1.0).animate(CurvedAnimation(
+      parent: _controllerAnimation,
+      curve: Curves.ease,
     ));
     // _heightAnimantion.addListener(() {
     //   setState(() {});
@@ -231,15 +236,14 @@ class _AuthCardState extends State<AuthCard>
         borderRadius: BorderRadius.circular(10.0),
       ),
       elevation: 8.0,
-      child: AnimatedBuilder(
-        animation: _heightAnimantion,
-        builder: (ctx, child) => Container(
-            height: _heightAnimantion.value.height,
-            constraints:
-                BoxConstraints(minHeight: _heightAnimantion.value.height),
-            width: deviceSize.width * 0.75,
-            padding: EdgeInsets.all(16.0),
-            child: child),
+      child: AnimatedContainer(
+        duration: Duration(milliseconds: 300),
+        curve: Curves.easeIn,
+        height: _authMode == AuthMode.Signup ? 320 : 260,
+        constraints:
+            BoxConstraints(minHeight: _authMode == AuthMode.Signup ? 320 : 260),
+        width: deviceSize.width * 0.75,
+        padding: EdgeInsets.all(16.0),
         child: Form(
           key: _formKey,
           child: SingleChildScrollView(
@@ -272,20 +276,33 @@ class _AuthCardState extends State<AuthCard>
                     _authData['password'] = value;
                   },
                 ),
-                if (_authMode == AuthMode.Signup)
-                  TextFormField(
-                    enabled: _authMode == AuthMode.Signup,
-                    decoration: InputDecoration(labelText: 'Confirm Password'),
-                    obscureText: true,
-                    validator: _authMode == AuthMode.Signup
-                        ? (value) {
-                            if (value != _passwordController.text) {
-                              return 'Passwords do not match!';
-                            }
-                            return null;
-                          }
-                        : null,
+                AnimatedContainer(
+                  constraints: BoxConstraints(
+                      minHeight: _authMode == AuthMode.Signup ? 60 : 0,
+                      maxHeight: _authMode == AuthMode.Signup ? 120 : 0),
+                  curve: Curves.ease,
+                  duration: Duration(milliseconds: 300),
+                  child: FadeTransition(
+                    opacity: _opacityAnimation,
+                    child: SlideTransition(
+                      position: _slideAnimantion,
+                      child: TextFormField(
+                        enabled: _authMode == AuthMode.Signup,
+                        decoration:
+                            InputDecoration(labelText: 'Confirm Password'),
+                        obscureText: true,
+                        validator: _authMode == AuthMode.Signup
+                            ? (value) {
+                                if (value != _passwordController.text) {
+                                  return 'Passwords do not match!';
+                                }
+                                return null;
+                              }
+                            : null,
+                      ),
+                    ),
                   ),
+                ),
                 SizedBox(
                   height: 20,
                 ),
@@ -318,6 +335,5 @@ class _AuthCardState extends State<AuthCard>
         ),
       ),
     );
-    ;
   }
 }
